5d5f9e9558dd3ddf343cb011cffedb79ef8970ea
AlessandroPozzi96 <alessandro.pozzi72@gmail.com>
2023-11-09 23:27:45 +0100

    Add image

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=2860f1e6ffe87ef68be2114017a2bc1e";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+export {BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL};
