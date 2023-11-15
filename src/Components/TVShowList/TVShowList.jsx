fb0ff11f894ba05f5a3e1b22de9fb5bd5c665661
AlessandroPozzi96 <alessandro.pozzi72@gmail.com>
2023-11-15 23:23:44 +0100

    Fix scroll

--- src/Components/TVShowList/TVShowList.jsx
+++ src/Components/TVShowList/TVShowList.jsx
@@ -2,10 +2,13 @@ import { TVShowListItem } from "../TVShowListItem/TVShowListItem";
 import s from "./style.module.css";
 
 export function TVShowList({ tvShowList }) {
+  const handleOnWheel = () => {
+    console.log("wheel");
+  };
   return (
     <>
       <div className={s.title}>You may also like:</div>
-      <div className={s.list}>
+      <div className={s.list} onWheel={handleOnWheel}>
         {tvShowList.map((tvShow) => {
           return (
             <span key={tvShow.id} className={s.tv_show_list_item}>
--- src/Components/TVShowListItem/TVShowListItem.jsx
+++ src/Components/TVShowListItem/TVShowListItem.jsx
@@ -4,11 +4,14 @@ import s from "./style.module.css";
 export function TVShowListItem({ tvShow, onClick }) {
   return (
     <div className={s.container} onClick={() => onClick(tvShow)}>
-      <img
-        className={s.image}
-        alt={tvShow.name}
-        src={SMALL_IMG_COVER_BASE_URL + tvShow.backdrop_path}
-      />
+      {tvShow.backdrop_path && (
+        <img
+          className={s.image}
+          alt={tvShow.name}
+          src={SMALL_IMG_COVER_BASE_URL + tvShow.backdrop_path}
+        />
+      )}
+      {!tvShow.backdrop_path && <div className={s.backboneImage} />}
       <div className={s.title}>{tvShow.name}</div>
     </div>
   );
